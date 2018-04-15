1a20a20fb6d69cb5e67ef17766256cefd39cef09
Yaiiloe <obj@oahic.xyz>
2018-04-15 22:00:58 +0800

    note

--- notes/snote.py
+++ notes/snote.py
@@ -1,12 +1,12 @@
 import time
 scale = 50
 print("------执行开始------")
-t = time.clock()
+start = time.clock()
 for i in range(scale + 1):
     a = "*" * i
     b = "." * (scale - i)
     c = (i /scale)*100
-    t -= time.clock()
+    t = time.clock() - start
     print("\r{:^3.0f}%[{}->{}]{:.2f}".format(c,a,b,t),end = "")
     time.sleep(0.05)
 print("\n------执行结束------")
